f7899a8680bc49d0e35aea031e0e77c09e56a176
Renata Bryczek <colibri.bailando@gmail.com>
2023-07-25 18:55:36 +0200

    M03L09
    JS - pierwsze kroki

---
+++ main.js
@@ -0,0 +1,9 @@
+const firstName = 'Renia';
+const age = 164
+
+
+console.log(firstName);
+console.log(age);
+console.log(`Nazywam się ${firstName} i mam ${age} lat`)
+
+//alert('! to strona moja, ale nie Twoja :)'); <-- to jest funkcja rzucająca tekstem na twarz i można kliknąć OK
