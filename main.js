ca37694e0ac066597161ae9018cc892993cfce68
Renata Bryczek <colibri.bailando@gmail.com>
2023-08-06 16:55:15 +0200

    BEM & DOM

--- main.js
+++ main.js
@@ -7,3 +7,15 @@ console.log(age);
 console.log(`Nazywam się ${firstName} i mam ${age} lat`)
 
 //alert('! to strona moja, ale nie Twoja :)'); <-- to jest funkcja rzucająca tekstem na twarz i można kliknąć OK
+
+const header = document.querySelector('.page-header__heading--js')
+
+console.log(header)
+console.log(header.innerHTML) //tu już się odnosimy do samej wartości
+
+//header.innerHTML = '.bez strąka stronka' <-- można skryptami zmienić treść
+
+console.log(header.innerHTML)
+
+//header.style.color = 'tomato' <-- można skryptami zmieniać style
+console.log(header.style)
